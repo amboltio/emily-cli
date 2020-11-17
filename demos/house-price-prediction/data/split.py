df9a434ce5c37d9a7d061df6fdbbecdf7820aadd
Mathias Ruggaard Pedersen <marupe@ambolt.io>
2020-11-17 13:46:31 +0100

    Renamed linear-regression demo to house-price-prediction

---
+++ demos/house-price-prediction/data/split.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+data = pd.read_csv("HousingData.csv")
+
+train, test = train_test_split(data, test_size=0.2)
+
+train.to_csv(r'train_data.csv', index=False)
+test.to_csv(r'test_data.csv', index=False)
