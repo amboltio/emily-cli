46eacbc91c930f97540906bd80fe85609fd227db
Anders Brams <abra@ambolt.io>
2020-12-21 16:44:56 +0100

    Added pretty buttons

--- demos/base/README.md
+++ demos/base/README.md
@@ -2,6 +2,9 @@
 
 These files define the base for an Emily demo to ensure consistent presentation.
 
+## Prerequisites
+A SCSS pre-compiler. In VS Code, install the `Live Sass Compiler` extension, press CMD + SHIFT + P and run `Live Sass: Watch Sass`. The extension will automatically compile the `*/**/*.scss` files into `.css` files when you change them.
+
 ## Quick start
 ```
 python3 -m http.server 8000
@@ -13,11 +16,17 @@ Open http://localhost:8000 in your browser.
 Your content goes into `index.html`:
 ```html
 ...
-        <section class="interaction">
+        <section class="content">
             <!-- INSERT CONTENT HERE -->
-            <p>[Content goes here]</p>
+
+            <h2>Your content goes here</h2>
+            ...
+
         </section>
-...
+... 
 ```
 
 ## Styling
+Declare your styling rules in `./styles/content.scss`. 
+Consult `./styles/variables/variables.scss` for a set of color variables that you can use.
+Consult `./styles/base/base.scss` for the base styling that is applied to the entire document.
--- demos/base/scripts/script.js
+++ demos/base/scripts/script.js
@@ -1,43 +1,9 @@
-const textArea = document.getElementById('input')
-const positiveScoreBar = document.getElementById('positive-sentiment-bar-fill')
-const emoji = document.getElementById('emoji')
+const clickMeBtn = document.getElementById('click-me-btn')
 
-const apiBaseUrl = 'http://localhost:4242'
+clickMeBtn.addEventListener('click', () => {
+    alert(`
+        You can add click handlers to buttons with
+        "btnElement.addEventListener('click', () => { ... })"
+    `)
+})
 
-const happyEmoji = '😀'
-const neutralEmoji = '😐'
-const angeryEmoji = '😠'
-
-const percentage = (value, sum) => (value / sum) * 100
-
-const updateSentimentScore = (pos) => {
-    positiveScoreBar.style.width = `${pos}%`
-}
-
-const updateEmoji = (pos) => {
-    if (pos > 65) {
-        emoji.innerText = happyEmoji
-    } else if (pos < 45) {
-        emoji.innerText = angeryEmoji
-    } else {
-        emoji.innerText = neutralEmoji
-    }
-}
-
-const getPrediction = async (text) => {
-    const response = await fetch(`${apiBaseUrl}/api/predict?sample=${text}`, { method: 'POST' })
-    const body = await response.json()
-    return body.prediction
-}
-
-const getSentiment = async () => {
-    const text = textArea.value
-    const { positive, negative } = await getPrediction(text)
-    if (positive == null) return
-    const total = positive + negative
-
-    const posPercentage = percentage(positive, total)
-
-    updateSentimentScore(posPercentage)
-    updateEmoji(posPercentage)
-}
