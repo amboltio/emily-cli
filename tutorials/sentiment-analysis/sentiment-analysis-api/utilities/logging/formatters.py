cb27b1a20bd9afc6813875636d2d2d50c24321c1
Christoffer Simonsen <christoffer@ambolt.io>
2021-08-04 12:26:03 +0200

    updated sentiment analysis to emily version 0.2.5

---
+++ tutorials/sentiment-analysis/sentiment-analysis-api/utilities/logging/formatters.py
@@ -0,0 +1,20 @@
+from pprint import pformat
+from loguru._defaults import LOGURU_FORMAT
+
+
+def single_line_format(record: dict) -> str:
+    """
+    Custom format for loguru loggers.
+    Uses pformat for log any data like request/response body during debug.
+    Works with logging if loguru handler it.
+    """
+
+    format_string = LOGURU_FORMAT
+    if record["extra"].get("payload") is not None:
+        record["extra"]["payload"] = pformat(
+            record["extra"]["payload"], indent=4, compact=True, width=88
+        )
+        format_string += "\n<level>{extra[payload]}</level>"
+
+    format_string += "{exception}\n"
+    return format_string
