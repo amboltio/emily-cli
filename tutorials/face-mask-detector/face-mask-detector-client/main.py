4dbc67ff8a14a8a41e5928ac0571efacd1a6a229
Chris H. Bahnsen <chris.bahnsen@hotmail.com>
2023-01-12 12:13:18 +0100

    Moved tutorials to https://github.com/amboltio/emily-tutorials/

--- tutorials/face-mask-detector/face-mask-detector-client/main.py
+++
@@ -1,9 +0,0 @@
-import http.server
-import socketserver
-
-PORT = 8000
-
-Handler = http.server.SimpleHTTPRequestHandler
-httpd = socketserver.TCPServer(("", PORT), Handler)
-print("serving at port", PORT)
-httpd.serve_forever()
